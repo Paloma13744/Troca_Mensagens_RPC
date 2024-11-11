d9ddbc931ed2702c94d97aef48cbc7f20c310ea7
Paloma <paloma13744@gmail.com>
2024-11-11 17:45:10 -0300

    Alguns comentários adicionados

--- clienteRPC.py
+++ clienteRPC.py
@@ -2,17 +2,17 @@ import rpyc
 
 class ChatClient:
     def __init__(self):
-        self.conn = rpyc.connect("localhost", 18861)  # Conecta ao servidor
+        self.conn = rpyc.connect("localhost", 18861)  # Conecta ao servidor , chama os serviços da aplicação
         self.identificacao = None
         self.sala_atual = None
 
-    def ingressar_no_sistema(self, nome):
+    def ingressar_no_sistema(self, nome):  # Função para inciar no sistema
         self.identificacao = self.conn.root.ingressar_no_sistema(nome)
-        print(f"Você foi registrado como: {self.identificacao}")
+        print(f"Usuário registrado(a) como: {self.identificacao}")
 
     def escolher_sala(self):
         while True:
-            sala = input("Escolha uma sala (sala1, sala2, sala3): ")
+            sala = input("Escolha uma sala:(sala1, sala2 ou sala3): ")
             if sala in ["sala1", "sala2", "sala3"]:
                 self.sala_atual = sala
                 resultado = self.conn.root.entrar_na_sala(self.identificacao, sala)
@@ -21,11 +21,11 @@ class ChatClient:
             else:
                 print("Sala inválida. Escolha entre sala1, sala2 ou sala3.")
 
-    def sair_da_sala(self):
+    def sair_da_sala(self): # Função para sair da sala e também do diciionário de usuários (remove)
         if self.sala_atual:
             resultado = self.conn.root.sair_da_sala(self.identificacao, self.sala_atual)
             print(resultado)
-            self.sala_atual = None
+            self.sala_atual = None   
         else:
             print("Você não está em nenhuma sala.")
 
@@ -83,7 +83,7 @@ class ChatClient:
             print("Você deve escolher uma sala para ver as identificações dos usuários.")
 
 
-    def menu(self):
+    def menu(self): # Função para inciar o menu
         nome = input("Digite seu nome: ")
         self.ingressar_no_sistema(nome)
         self.escolher_sala()
@@ -91,37 +91,33 @@ class ChatClient:
         while True:
             print("\nMenu:")
             print("1 - Enviar mensagem pública")
-            print("2 - Listar mensagens públicas")
+            print("2 - Mostrar mensagens públicas")
             print("3 - Enviar mensagem privada")
-            print("4 - Listar mensagens privadas")
-            print("5 - Listar usuários na sala")
-            print("6 - Sair da sala")
-            print("7 - Sair do sistema")
-            opcao = input("Escolha uma opção: ")
+            print("4 - Mostrar mensagens privadas")
+            print("5 - Mostrar usuários na sala")
+            print("6 - Sair ")
+            opcao = input("Escolha uma opção:")
 
             if opcao == "1":
-                mensagem = input("Digite a mensagem pública: ")
-                self.enviar_mensagem_publica(mensagem)
+                mensagem = input("Digite a mensagem: ")
+                self.enviar_mensagem_publica(mensagem)  # -> Envia mensagem publicamente para todos os que estão na mesma sala
             elif opcao == "2":
-                self.listar_mensagens_publicas()
+                self.listar_mensagens_publicas()  # -> Mostra todas as mensagens dos usuários que estão na mesma sala
             elif opcao == "3": 
                 self.listar_identificacoes_usuarios()  
                 destinatario = input("Digite o identificador do destinatário: ")
-                mensagem = input("Digite a mensagem privada: ")
-                self.enviar_mensagem_privada(destinatario, mensagem)
+                mensagem = input("Digite a mensagem: ")
+                self.enviar_mensagem_privada(destinatario, mensagem)  # -> Envia mensagem para um usuário específico (na mesma sala)
             elif opcao == "4":
-                self.listar_mensagens_privadas()
+                self.listar_mensagens_privadas()  # -> Mostra as mensagens para um usuário específico (na mesma sala)
             elif opcao == "5":
-                self.listar_usuarios()
+                self.listar_usuarios()    # -> Lista os usuários que estão na mesma sala
             elif opcao == "6":
-                self.sair_da_sala()
-                self.escolher_sala()
-            elif opcao == "7":
-                print("Saindo do sistema.")
-                break
+                self.sair_da_sala()    # -> Sai da sala e também do dicionário de usuários 
+                break                 # Sai do sistema de bate-papo
             else:
-                print("Opção inválida.")
+                print("Opção inválida. Tente as opções: 1,2,3,4,5 ou 6")
 
-if __name__ == "__main__":
-    cliente = ChatClient()
+if __name__ == "__main__":   # Inicia a main dos usuários
+    cliente = ChatClient() 
     cliente.menu()
